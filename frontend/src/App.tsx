83a4cdf908cbd16821aea762f328bd40df05384b
Abel Cañas <abelstor@gmail.com>
2026-07-03 16:37:05 +0200

    chore: create basic structure for backend and frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1>Proyecto de Clasificación</h1>
+      <Button>Click me</Button>
+    </div>
+    </>
+  );
+}
+
+export default App;
