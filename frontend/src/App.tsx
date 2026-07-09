e63f11fd578d645244ae035d82afc3590f663429
gisella <gisellcuestah@gmail.com>
2026-07-09 15:29:24 +0200

    Guardando cambios antes de cambiar a dev

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
-import { Button } from '@/components/ui/button';
-import './App.css';
+import { Button } from "@/components/ui/button";
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <>
+      {/* Código de tu compañero */}
       <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1>Proyecto de Clasificación</h1>
-      <Button>Click me</Button>
-    </div>
+        <h1>Proyecto de Clasificación</h1>
+        <Button>Click me</Button>
+      </div>
+
+      {/* Tu Dashboard debajo */}
+      <Dashboard />
     </>
   );
 }
