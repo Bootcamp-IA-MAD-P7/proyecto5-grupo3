546230e45b0518352156031b6b084dce3c00b585
gisella <gisellcuestah@gmail.com>
2026-07-10 09:21:36 +0200

    Guardando mis cambios del Dashboard antes de actualizar la rama dev

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-2xl font-bold">ChurnPrediction Dashboard</h1>
+
+      {/* Aquí irán tus componentes */}
+    </div>
+  );
+}
